95d8ba9c189d878a3c0890a2b775b8d188b6e548
mariusionut03 <49335973+mariusionut03@users.noreply.github.com>
2021-07-03 00:05:45 +0300

    Update v0.004
    OnTouchListener for every button from Home. Nice effect for Action Down or Up.

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,7 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 public class HomeFragment extends Fragment {
-    private Button learnButton;
+    private Button learnButton, quizButton, subiecteButton, settingsButton;
 
     @Nullable
     @Override
@@ -20,18 +22,73 @@ public class HomeFragment extends Fragment {
                 container, false);
 
         learnButton = (Button) view.findViewById(R.id.buttonLearn);
-        learnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setButtonTextColor();
+        learnButton.setOnClickListener(v -> learnButtonFunction());
+
+        quizButton = (Button) view.findViewById(R.id.buttonQuiz);
+        subiecteButton = (Button) view.findViewById(R.id.buttonSubiecte);
+        settingsButton = (Button) view.findViewById(R.id.buttonSettings);
+
+        learnButton.setOnTouchListener((v, event) -> {
+            switch ( event.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    learnButton.setTextColor(Color.parseColor("#FFFFFF"));
+                    learnButton.setBackgroundResource(R.drawable.buttonblack);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    learnButton.setTextColor(Color.parseColor("#000000"));
+                    learnButton.setBackgroundResource(R.drawable.buttonwhite);
+                    break;
             }
+            return true;
         });
+
+        quizButton.setOnTouchListener((v, event) -> {
+            switch ( event.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    quizButton.setTextColor(Color.parseColor("#FFFFFF"));
+                    quizButton.setBackgroundResource(R.drawable.buttonblack);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    quizButton.setTextColor(Color.parseColor("#000000"));
+                    quizButton.setBackgroundResource(R.drawable.buttonwhite);
+                    break;
+            }
+            return true;
+        });
+
+        subiecteButton.setOnTouchListener((v, event) -> {
+            switch ( event.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    subiecteButton.setTextColor(Color.parseColor("#FFFFFF"));
+                    subiecteButton.setBackgroundResource(R.drawable.buttonblack);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    subiecteButton.setTextColor(Color.parseColor("#000000"));
+                    subiecteButton.setBackgroundResource(R.drawable.buttonwhite);
+                    break;
+            }
+            return true;
+        });
+
+        settingsButton.setOnTouchListener((v, event) -> {
+            switch ( event.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    settingsButton.setTextColor(Color.parseColor("#FFFFFF"));
+                    settingsButton.setBackgroundResource(R.drawable.buttonblack);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    settingsButton.setTextColor(Color.parseColor("#000000"));
+                    settingsButton.setBackgroundResource(R.drawable.buttonwhite);
+                    break;
+            }
+            return true;
+        });
+
         return view;
     }
 
-    public void setButtonTextColor()
+    public void learnButtonFunction()
     {
-        learnButton.setTextColor(000000);
-
+        //
     }
 }
