b860c2c2c90b17460a25052d02d0ba0cf9f2d590
mariusionut03 <49335973+mariusionut03@users.noreply.github.com>
2021-07-07 20:25:52 +0300

    Update v0.011 (!)
    1. Created 'url_array.xml' to store the paths to HTML files.
    2. Created 5 new example.html files.
    3. Wrote the code in LearnActivity1.java to switch pages, jump between chapter, store the current chapter and page to load them when Learn Activity is opened.(current chapter and page are stored with SharedPreferences.
    4. Bottom page count in "activity_learn1.xml" is now changing depending on current page and current chapter total pages.

--- app/src/main/java/com/example/myapplication/LearnActivity1.java
+++ app/src/main/java/com/example/myapplication/LearnActivity1.java
@@ -1,67 +1,135 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LearnActivity1 extends AppCompatActivity {
 
-    Button buttonNext, buttonBack;
-    WebView webView;
+    public Button buttonNext, buttonBack, exitButton;
+    public TextView pageCountText;
+    public WebView webView;
+    public String[][] urlArray = new String[17][9];
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String THEME = "switchTheme";
+    public static final String CHAPTER = "chapter";
+    public static final String PAGE = "page";
+    private boolean theme;
+    private String chapterString, pageString;
+    private Integer chapter, page, chaptersCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn1);
 
+        // Load chapter and page with SharedPreferences
+        loadDataLearn();
+
+        // URL and pages Data
+        Resources res = getResources();
+        urlArray[0] = res.getStringArray(R.array.urlChapter1);
+        urlArray[1] = res.getStringArray(R.array.urlChapter2);
+        chaptersCount = res.getInteger(R.integer.chaptersCount);
+
         // WebView Setup
         webView = findViewById(R.id.webViewHtml);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/example01.html");
+        webView.loadUrl(urlArray[chapter][page]);
 
         // Exit Button (Top Left Corner)
-        Button exitButton;
         exitButton = findViewById(R.id.buttonExit);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        exitButton.setOnClickListener(v -> finish());
         // Next page Button
         buttonNext = findViewById(R.id.buttonNext);
-        buttonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buttonNextFunction();
-            }
-        });
+        buttonNext.setOnClickListener(v -> buttonNextFunction());
         // Back page Button
         buttonBack = findViewById(R.id.buttonBack);
-        buttonBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buttonBackFunction();
-            }
-        });
+        buttonBack.setOnClickListener(v -> buttonBackFunction());
+        // Page Count TextView (bottom of the page)
+        pageCountText = findViewById(R.id.textViewPageCount);
+        setPageCountTextFunction();
     }
 
     public void buttonNextFunction()
     {
-
+        if(page == urlArray[chapter].length - 1 && chapter != chaptersCount-1)
+        {
+            chapter++;
+            page = 0;
+            saveDataLearn();
+            setPageCountTextFunction();
+        }
+        else if(page == urlArray[chapter].length - 1 && chapter == chaptersCount-1)
+        {
+            saveDataLearn();
+            finish();
+        }
+        else
+        {
+            page++;
+            saveDataLearn();
+            setPageCountTextFunction();
+        }
+        webView.loadUrl(urlArray[chapter][page]);
     }
 
     public void buttonBackFunction()
     {
+        if(page == 0 && chapter != 0)
+        {
+            chapter--;
+            page = urlArray[chapter].length - 1;
+            saveDataLearn();
+            setPageCountTextFunction();
+        }
+        else if(page == 0 && chapter == 0)
+        {
+            saveDataLearn();
+            finish();
+        }
+        else
+        {
+            page--;
+            saveDataLearn();
+            setPageCountTextFunction();
+        }
+        webView.loadUrl(urlArray[chapter][page]);
+    }
 
+    public void loadDataLearn()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        theme = sharedPreferences.getBoolean(THEME, false);
+        chapterString = sharedPreferences.getString(CHAPTER, "0");
+        pageString = sharedPreferences.getString(PAGE, "0");
+        chapter = Integer.parseInt(chapterString);
+        page = Integer.parseInt(pageString);
+    }
+
+    public void saveDataLearn()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(CHAPTER, String.valueOf(chapter));
+        editor.putString(PAGE, String.valueOf(page));
+        editor.apply();
+    }
+
+    public void setPageCountTextFunction()
+    {
+        String pageCurrentString = String.valueOf(page+1);
+        String chapterCurrentString = String.valueOf(urlArray[chapter].length);
+        String textString = pageCurrentString + "/" + chapterCurrentString;
+        pageCountText.setText(textString);
     }
 }
