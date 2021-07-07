c464fc2f599e1df4d17cf7ef42c43690426e11da
mariusionut03 <ionutteq222@gmail.com>
2021-07-07 16:18:10 +0300

    Update v0.010
    1. Deleted 'examplewebview.html'.
    2. Added 3 html files: 'example01.html', 'example02.html' and 'example03.html'.
    3. Renamed 'backButton' to 'exitButton' and 'forwardsButton', 'backwardsButton' to 'nextButton', 'backButton' in activity_learn1.xml.
    4. Added 2 'OnClickListener' and functions for 'nextButton' and 'backButton' in LearnActivity1.java.

--- app/src/main/java/com/example/myapplication/LearnActivity1.java
+++ app/src/main/java/com/example/myapplication/LearnActivity1.java
@@ -13,27 +13,55 @@ import android.widget.Button;
 
 public class LearnActivity1 extends AppCompatActivity {
 
+    Button buttonNext, buttonBack;
+    WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn1);
 
-        WebView webView = findViewById(R.id.webViewHtml);
-
+        // WebView Setup
+        webView = findViewById(R.id.webViewHtml);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/example01.html");
 
-        webView.loadUrl("file:///android_asset/examplewebview.html");
-
-
-        Button backButton;
-        backButton = findViewById(R.id.buttonBack);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        // Exit Button (Top Left Corner)
+        Button exitButton;
+        exitButton = findViewById(R.id.buttonExit);
+        exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        // Next page Button
+        buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonNextFunction();
+            }
+        });
+        // Back page Button
+        buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonBackFunction();
+            }
+        });
     }
 
+    public void buttonNextFunction()
+    {
+
+    }
+
+    public void buttonBackFunction()
+    {
+
+    }
 }
